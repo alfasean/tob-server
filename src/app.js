5788aef846aae0975b550e72f003aa96086ebe64
Alfa Sean <alfasean22@gmail.com>
2024-11-14 13:59:27 +0700

    migrate from mysql to pg

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
 const policyRoutes = require('./routes/policyRoutes');
-const mysql = require('mysql2');
 require('dotenv').config();
+const { Pool } = require('pg');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,26 +11,16 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_ADDON_HOST,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD,
-  database: process.env.MYSQL_ADDON_DB,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 app.use((req, res, next) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error connecting to database:', err);
-      res.status(500).send('Error connecting to database');
-    } else {
-      req.pool = connection;
-      next();
-    }
-  });
+  req.pool = pool;
+  next();
 });
 
 app.use('/api/policies', policyRoutes);
--- src/controllers/policyController.js
+++ src/controllers/policyController.js
@@ -1,15 +1,19 @@
-const db = require('../models/policyModel');
+const pool = require('../models/policyModel');
 
 exports.createPolicy = (req, res) => {
   const { insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate } = req.body;
   const policy_number = `POL${new Date().getTime()}`;
   const premium_price = vehicle_price * (premium_rate / 100);
 
-  const query = 'INSERT INTO policies (policy_number, insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate, premium_price) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  const query = `
+    INSERT INTO policies (policy_number, insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate, premium_price)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+  `;
+  const values = [policy_number, insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate, premium_price];
 
-  req.pool.query(query, [policy_number, insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate, premium_price], (err, results) => {
+  pool.query(query, values, (err, result) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err.message);
     }
     res.status(201).send('Policy created successfully');
   });
@@ -17,11 +21,22 @@ exports.createPolicy = (req, res) => {
 
 exports.getAllPolicies = (req, res) => {
   const query = 'SELECT * FROM policies';
-  req.pool.query(query, (err, results) => {
+  pool.query(query, (err, result) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err.message);
     }
-    res.status(200).json(results);
+    res.status(200).json(result.rows);
+  });
+};
+
+exports.getPolicyById = (req, res) => {
+  const { id } = req.params;
+  const query = 'SELECT * FROM policies WHERE id = $1';
+  pool.query(query, [id], (err, result) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.status(200).json(result.rows[0]);
   });
 };
 
@@ -30,11 +45,16 @@ exports.updatePolicy = (req, res) => {
   const { insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate } = req.body;
   const premium_price = vehicle_price * (premium_rate / 100);
 
-  const query = 'UPDATE policies SET insured_name = ?, effective_date = ?, expiry_date = ?, vehicle_brand = ?, vehicle_type = ?, vehicle_year = ?, vehicle_price = ?, premium_rate = ?, premium_price = ? WHERE id = ?';
+  const query = `
+    UPDATE policies
+    SET insured_name = $1, effective_date = $2, expiry_date = $3, vehicle_brand = $4, vehicle_type = $5, vehicle_year = $6, vehicle_price = $7, premium_rate = $8, premium_price = $9
+    WHERE id = $10
+  `;
+  const values = [insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate, premium_price, id];
 
-  req.pool.query(query, [insured_name, effective_date, expiry_date, vehicle_brand, vehicle_type, vehicle_year, vehicle_price, premium_rate, premium_price, id], (err, results) => {
+  pool.query(query, values, (err, result) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err.message);
     }
     res.status(200).send('Policy updated successfully');
   });
@@ -42,31 +62,16 @@ exports.updatePolicy = (req, res) => {
 
 exports.deletePolicy = (req, res) => {
   const { id } = req.params;
+  const query = 'DELETE FROM policies WHERE id = $1';
 
-  const query = 'DELETE FROM policies WHERE id = ?';
-
-  req.pool.query(query, [id], (err, results) => {
+  pool.query(query, [id], (err, result) => {
     if (err) {
-      return res.status(500).send(err);
+      return res.status(500).send(err.message);
     }
     res.status(200).send('Policy deleted successfully');
-  });
-};
-
-exports.getPolicyById = (req, res) => {
-  const { id } = req.params;
-
-  const query = 'SELECT * FROM policies WHERE id = ?';
-
-  req.pool.query(query, [id], (err, results) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.status(200).json(results);
   });
 };
 
 exports.favicon = (req, res) => {
   res.sendStatus(204);
 };
-
